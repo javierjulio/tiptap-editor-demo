bcb479eba13eea2fcd41e2de9c083a3faf2d68ae
Javier Julio <jjfutbol@gmail.com>
2024-07-21 18:07:52 -0400

    Add base to fix GitHub pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/tiptap-editor-demo/' // use repo name https://vitejs.dev/guide/static-deploy.html#github-pages
 })
