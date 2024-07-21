6a3496d8f8998702cbe42aba46744370e9abdcf9
Javier Julio <jjfutbol@gmail.com>
2024-07-21 18:08:26 -0400

    Open in browser by default for dev

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    // host: true, // Enable to expose host for running app on phone
+    open: true
+  },
   base: '/tiptap-editor-demo/' // use repo name https://vitejs.dev/guide/static-deploy.html#github-pages
 })
